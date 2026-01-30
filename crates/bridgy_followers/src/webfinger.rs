7e3ac122fe78396ceb42fd0caccebd3c2b943c88
Julien Roncaglia <julien@roncaglia.fr>
2026-01-30 11:47:52 +0100

    Fix webfinger

--- crates/bridgy_followers/src/webfinger.rs
+++ crates/bridgy_followers/src/webfinger.rs
@@ -1,19 +1,23 @@
 use color_eyre::eyre::Result;
+use tracing::{debug, instrument};
 
+#[instrument()]
 pub async fn account_exists(domain: &str, acct: &str) -> Result<bool> {
     let resource = format!("acct:{}", acct);
 
     let url = {
-        let mut url = reqwest::Url::parse(domain)?
-            .join(".well-known")?
-            .join("webfinger")?;
+        let mut url = reqwest::Url::parse(domain)?;
+        url.set_path(".well-known/webfinger");
+
         let mut query_pairs = url.query_pairs_mut();
         query_pairs.append_pair("resource", &resource);
         drop(query_pairs);
         url
     };
 
+    debug!("Fetching WebFinger URL: {}", url);
     let response = reqwest::get(url).await?;
+    let status = response.status();
 
-    Ok(response.status().is_success())
+    Ok(status.is_success())
 }
