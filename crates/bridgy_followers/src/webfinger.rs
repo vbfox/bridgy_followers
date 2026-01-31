854beb301f2cc26af8a7567062de8c63706719fe
Julien Roncaglia <julien@roncaglia.fr>
2026-01-31 15:58:50 +0100

    Split webfinger code

--- crates/bridgy_followers/src/webfinger.rs
+++ crates/bridgy_followers/src/webfinger.rs
@@ -1,19 +1,22 @@
 use color_eyre::eyre::Result;
+use reqwest::Url;
 use tracing::{debug, instrument};
+
+fn get_domain_webfinger_url(domain: &str, resource: &str) -> Result<Url, url::ParseError> {
+    let mut url = Url::parse(domain)?;
+    url.set_path(".well-known/webfinger");
+
+    let mut query_pairs = url.query_pairs_mut();
+    query_pairs.append_pair("resource", &resource);
+    drop(query_pairs);
+
+    Ok(url)
+}
 
 #[instrument()]
 pub async fn account_exists(domain: &str, acct: &str) -> Result<bool> {
     let resource = format!("acct:{}", acct);
-
-    let url = {
-        let mut url = reqwest::Url::parse(domain)?;
-        url.set_path(".well-known/webfinger");
-
-        let mut query_pairs = url.query_pairs_mut();
-        query_pairs.append_pair("resource", &resource);
-        drop(query_pairs);
-        url
-    };
+    let url = get_domain_webfinger_url(domain, &resource)?;
 
     debug!("Fetching WebFinger URL: {}", url);
     let response = reqwest::get(url).await?;
