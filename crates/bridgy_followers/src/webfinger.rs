5b1b7a510eb4c556036513e54d2318de2b58899d
Julien Roncaglia <julien@roncaglia.fr>
2026-01-31 16:02:10 +0100

    Add relevant RFCs for webfinger

--- crates/bridgy_followers/src/webfinger.rs
+++ crates/bridgy_followers/src/webfinger.rs
@@ -2,6 +2,9 @@ use color_eyre::eyre::Result;
 use reqwest::Url;
 use tracing::{debug, instrument};
 
+/// Construct the WebFinger URL for a given domain and resource according to [RFC 7033][rfc7033].
+///
+/// [rfc7033]: https://datatracker.ietf.org/doc/html/rfc7033
 fn get_domain_webfinger_url(domain: &str, resource: &str) -> Result<Url, url::ParseError> {
     let mut url = Url::parse(domain)?;
     url.set_path(".well-known/webfinger");
@@ -13,6 +16,10 @@ fn get_domain_webfinger_url(domain: &str, resource: &str) -> Result<Url, url::Pa
     Ok(url)
 }
 
+/// Uses WebFinger (RFC [7565][rfc7565]) to check if an account exists on a given
+/// domain using its acct URI.
+///
+/// [rfc7565]: https://datatracker.ietf.org/doc/html/rfc7565
 #[instrument()]
 pub async fn account_exists(domain: &str, acct: &str) -> Result<bool> {
     let resource = format!("acct:{}", acct);
